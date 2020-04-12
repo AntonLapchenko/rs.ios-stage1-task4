dd0635e087aff930ab4d1878f220449c997bce67
TatyanaShut <tatyana.shut.solveit@gmail.com>
2020-04-12 15:05:47 +0300

    feat: add Task4

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,28 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    NSMutableArray *result = [NSMutableArray new];
+    [result addObjectsFromArray:[self decomposer:[number integerValue] and:[number integerValue] * [number integerValue]]];
+    if (result == nil) {
+        return nil;
+    } else {
+        return result;
+    }
+}
+
+-(NSMutableArray*)decomposer:(NSInteger)n and:(NSInteger)remain{
+    if (remain == 0) {
+        return nil;
+    }
+    for (NSInteger i = n - 1; i > 0; i--) {
+        if ((remain - i * i) >= 0) {
+            NSMutableArray *result = [[NSMutableArray alloc] initWithArray:[self decomposer: i and:(remain - i * i)]];
+            if (result != nil) {
+                [result addObject:[NSNumber numberWithInteger:n]];
+                return result;
+            }
+        }
+    }
+    return nil;
 }
 @end
